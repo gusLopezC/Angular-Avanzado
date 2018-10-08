e6251823e5b256ac5f4e42a2ba17b6e0b96ce6f3
Gustavo <gcallejas@grupoeducare.com>
2018-10-08 13:54:34 -0500

    Se termian los rxs y el nombre de las paginas

--- src/app/pages/rxjs/rxjs.component.ts
+++ src/app/pages/rxjs/rxjs.component.ts
@@ -1,41 +1,84 @@
-import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Observable, Subscriber, Subscription } from 'rxjs';
+import { retry, map, filter } from 'rxjs/operators';
 
 @Component({
-  selector: 'app-rxjs',
-  templateUrl: './rxjs.component.html',
-  styles: []
+    selector: 'app-rxjs',
+    templateUrl: './rxjs.component.html',
+    styles: []
 })
-export class RxjsComponent implements OnInit {
+export class RxjsComponent implements OnInit , OnDestroy {
 
-  constructor() {
+  subscription: Subscription;
 
-  const obs = new Observable( observer => {
+    constructor() {
+        /*
+        * @note Metodo Retry define cuantos intentos se repetira el obersable empezando del segundo ciclo
+          this.regresaObservable().pipe(
+            retry(3)
+          ).subscribe(
+            numero => console.log('Subs', numero.toExponential),
+            error => console.error('Error en el obs', error),
+            () => console.log('El observador termino')
+          );
+        }*/
 
-    let contador = 0;
+        // * @note Metodo Map convierte un objeto a un valor permite trabajar con menos codigo
+       this.subscription = this.regresaObservable()
+            .subscribe(
+                numero => console.log('Subs', numero),
+                error => console.error('Error en el obs', error),
+                () => console.log('El observador termino')
+            );
+    }
 
-    const intervalo = setInterval(() => {
+    ngOnInit() {
+    }
 
-      contador += 1;
-      observer.next(contador);
-      if ( contador === 3) {
-        clearInterval();
-        observer.complete();
-      } if ( contador === 3) {
-        observer.error('Ya fue');
-      }
+    ngOnDestroy() {
+      console.log('La pagina se va a cerrar');
+      this.subscription.unsubscribe();
+    }
 
-    }, 1000);
-  });
+    regresaObservable(): Observable<any> {
 
-  obs.subscribe(
-    numero =>  console.log('Subs', numero),
-    error =>  console.error('Error en el obs', error),
-    () => console.log('El observador termino')
-  );
-  }
+        return new Observable((observer: Subscriber<any>) => {
 
-  ngOnInit() {
-  }
+            let contador = 0;
 
+            const intervalo = setInterval(() => {
+
+                contador++;
+
+                const salida = {
+                    valor: contador
+                };
+                observer.next(salida);
+
+                // if (contador === 3) {
+                //     clearInterval(intervalo);
+                //     observer.complete();
+                // }
+                // if (contador === 2) {
+                //   // clearInterval(intervalo);
+                //   observer.error('Ya fue');
+                // }
+
+            }, 1000);
+        }).pipe(
+            map(resp => resp.valor),
+            /**
+            * Metodo filter muestra filtra un solo resultado tiene que devolver true o false
+            */
+
+            filter((valor, index) => {
+                // console.log('filter', valor, index);
+                if ((valor % 2 === 1)) {
+                    return true;
+                } else {
+                    return false;
+                }
+            })
+        );
+    }
 }
